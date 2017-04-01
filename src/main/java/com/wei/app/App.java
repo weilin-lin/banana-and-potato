b85695dcd9897c6cb7a844a2ee9f595aeebbc5a3
panggua <panggua@gmail.com>
2017-04-01 21:41:31 +0900

    basic echo function

--- src/main/java/com/wei/app/App.java
+++ src/main/java/com/wei/app/App.java
@@ -1,13 +1,48 @@
 package com.wei.app;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+
+import com.linecorp.bot.model.event.message.TextMessageContent;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+	public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
+  @Autowired
+  private LineMessagingService lineMessagingService;
+
+  @EventMapping
+  public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+    System.out.println("event: " + event);
+    final BotApiResponse apiResponse = lineMessagingService
+        .replyMessage(new ReplyMessage(event.getReplyToken(),
+                                       (Message) Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+        .execute().body();
+    System.out.println("Sent a message: " + apiResponse);
+  }
+
+  @EventMapping
+  public void defaultMessageEvent(Event event) {
+    System.out.println("event: " + event);
+  }
 }
