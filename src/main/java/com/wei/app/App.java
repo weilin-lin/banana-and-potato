6e1cb82e6353275d2b35988f4f8763e608a6314b
panggua <panggua@gmail.com>
2017-04-02 19:34:48 +0900

    added a small watson code_3

--- src/main/java/com/wei/app/App.java
+++ src/main/java/com/wei/app/App.java
@@ -84,7 +84,7 @@ public class App {
 		log.info("Got text message from {}: {}, userID = {}", replyToken, text, event.getSource().getUserId());
 		switch (text) {
 		case "Nini": {
-			String answer = this.conversation("hi");
+			//String answer = this.conversation("hi");
 			/*String userId = event.getSource().getUserId();
 			if (userId != null) {
 				Response<UserProfileResponse> response = lineMessagingService.getProfile(userId).execute();
@@ -99,7 +99,7 @@ public class App {
 				this.replyText(replyToken, "Bot can't use profile API without user ID");
 			}*/
 			this.replyText(replyToken, "\\^O^/");
-			this.replyText(replyToken, answer);
+			//this.replyText(replyToken, answer);
 			break;
 		}
 		case "bye": {
@@ -232,7 +232,7 @@ public class App {
 		System.out.println("event: " + event);
 	}
 	
-	public String conversation(String textToWatson) {
+	/*public String conversation(String textToWatson) {
 		
 		ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_07_11);
 		service.setUsernameAndPassword("41e6bfc4-912f-4ce4-bc49-ddeb421ed65a","dJ2r5NHnTTS7");
@@ -241,5 +241,5 @@ public class App {
 		MessageResponse response = service.message("58351ed8-a0f5-4746-8dcf-0d49e1a71bdf", newMessage).execute();
 		System.out.println(response);
 		return response.toString();
-	}
+	}*/
 }
